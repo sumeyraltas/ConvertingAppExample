314700e7b8a5b7fe462b0ff3c27486a276bf6679
sumeyraltas <sumeyraltas@gmail.com>
2023-11-06 17:25:54 +0300

    Initial Commit

---
+++ ConvertingAppExample/ConvertingAppExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConvertingAppExampleApp.swift
+//  ConvertingAppExample
+//
+//  Created by Sumeyra Altas on 6.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct ConvertingAppExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
