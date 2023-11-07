bc26bdecfbe945b855e1075c4c790452cf63a38c
sumeyraltas <sumeyraltas@gmail.com>
2023-11-07 12:11:59 +0300

    Conversion operations are carried out with a basic design.
    The design can be improved and the conversion type can be diversified.

--- ConvertingAppExample/ContentView.swift
+++ ConvertingAppExample/ContentView.swift
@@ -1,24 +1,72 @@
-//
-//  ContentView.swift
-//  ConvertingAppExample
-//
-//  Created by Sumeyra Altas on 6.11.2023.
-//
-
 import SwiftUI
 
+enum ConversionType: String, CaseIterable {
+    case celsiusToFahrenheit = "Celsius to Fahrenheit"
+    case fahrenheitToCelsius = "Fahrenheit to Celsius"
+    case cmTomm = "Centimeters to Millimeters"
+    case mmTocm = "Millimeters to Centimeters"
+    case mmTokm = "Millimeters to Kilometers"
+    case kmTomm = "Kilometers to Millimeters"
+    case inchesToCentimeters = "Inches to Centimeters"
+    case centimetersToInches = "Centimeters to Inches"
+    
+}
+
 struct ContentView: View {
+    @State private var input = ""
+    @State private var output = ""
+    @State private var selectedConversionType = ConversionType.celsiusToFahrenheit
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        Form {
+            Section(header: Text("Conversion APP").font(.system(size: 25, weight: .bold, design: .rounded))) {
+                List{
+                    Picker("Select the Conversion Type", selection: $selectedConversionType) {
+                        ForEach(ConversionType.allCases, id: \.self) { type in
+                            Text(type.rawValue)
+                        }
+                    }}
+                TextField("Enter your value", text: $input)
+                    .keyboardType(.decimalPad)
+                Button("Convert") {
+                    convertValues()
+                }
+                
+                Text("Result: \(output)")
+            }
         }
-        .padding()
+    }
+
+    func convertValues() {
+        guard let input = Double(input) else {
+            output = "Invalid input"
+            return
+        }
+
+        switch selectedConversionType {
+        case .celsiusToFahrenheit:
+            output = "\(input * 9/5 + 32) °F"
+        case .fahrenheitToCelsius:
+            output = "\((input - 32) * 5/9) °C"
+        case .cmTomm:
+            output = "\((input) * 10) mm"
+        case .mmTocm:
+            output = "\((input) / 10) cm"
+        case .kmTomm:
+            output = "\((input) * 1000) mm"
+        case .mmTokm:
+            output = "\((input) / 1000) km"
+        case .inchesToCentimeters:
+            output = "\((input) * 2.54) cm"
+        case .centimetersToInches:
+            output = "\((input) / 2.54) inch"
+        }
     }
 }
 
 #Preview {
     ContentView()
 }
+       
+
